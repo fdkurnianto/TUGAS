aaf6f91e5464c02fd0948daf61abfcfa4aae3015
fdkurnianto <132898822+fdkurnianto@users.noreply.github.com>
2023-05-11 22:43:49 +0700

    Add files via upload

---
+++ Tugas 2/JawabanSoal3.py
@@ -0,0 +1,11 @@
+angka = input('Enter a starting number (greater than 0) or QUIT:')
+ 
+n = int(angka)
+
+while n != 1:
+    if n % 2 == 0:  
+        n = n // 2
+    else:  
+        n = 3 * n + 1
+    print(', ' + str(n))
+    
